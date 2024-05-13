66f0c25969cffcd97dc81ec3b5bec3784d62921e
ganeshchape09 <ganeshchape09@gmail.com>
2024-05-14 03:29:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const connectDB = require("./config/db");
+const router = require("./routes");
+const path = require("path");
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", router);
+
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
+
+const PORT = 8080 || process.env.PORT;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("connnect to DB");
+    console.log("Server is running " + PORT);
+  });
+});
